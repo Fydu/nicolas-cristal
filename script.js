82aca5933dd886da41703d74b745215596eb8fed
Nicolas Cristal <103908372+nicocristal@users.noreply.github.com>
2023-09-18 12:04:23 -0300

    optimized lines of code in js

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-var ListVisibility;
 function showList() {
-    if (ListVisibility == 1) {
-        document.querySelector(".main-list").style.display="none";
-        document.getElementById("toggleBtnIcon").classList.add("fa-chevron-down")
-        document.getElementById("toggleBtnIcon").classList.remove("fa-chevron-up")
-        return ListVisibility = 0;
-    }
-    else {
-        document.querySelector(".main-list").style.display="block";
-        document.getElementById("toggleBtnIcon").classList.remove("fa-chevron-down")
-        document.getElementById("toggleBtnIcon").classList.add("fa-chevron-up")
-        return ListVisibility = 1;
-    }
-}
+    document.querySelector(".main-list").classList.toggle('active');
+};
 
-var ProjectsVisibility;
 function showProjects() {
-    if (ProjectsVisibility == 1) {
-        document.querySelector(".projects-div").style.display="none";
-        return ListVisibility = 0;
-    }
-    else {
-        document.querySelector(".projects-div").style.display="flex";
-        return ListVisibility = 1;
-    }
-}
+    document.querySelector(".projects-div").classList.toggle('active');
+};
