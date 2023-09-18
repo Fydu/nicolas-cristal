d9147dc8d7916dd68b8138331c3af8c113c5c85c
Nicolas Cristal <103908372+nicocristal@users.noreply.github.com>
2023-09-18 00:40:48 -0300

    added projects

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var visibility;
-function show_hide() {
+function showList() {
     if (visibility == 1) {
-        document.querySelector(".block ul").style.display="none";
-        document.getElementById("toggleBtn").classList.add("fa-chevron-down")
-        document.getElementById("toggleBtn").classList.remove("fa-chevron-up")
+        document.querySelector(".main-list").style.display="none";
+        document.getElementById("toggleBtnIcon").classList.add("fa-chevron-down")
+        document.getElementById("toggleBtnIcon").classList.remove("fa-chevron-up")
         return visibility = 0;
     }
     else {
-        document.querySelector(".block ul").style.display="block";
-        document.getElementById("toggleBtn").classList.remove("fa-chevron-down")
-        document.getElementById("toggleBtn").classList.add("fa-chevron-up")
+        document.querySelector(".main-list").style.display="block";
+        document.getElementById("toggleBtnIcon").classList.remove("fa-chevron-down")
+        document.getElementById("toggleBtnIcon").classList.add("fa-chevron-up")
         return visibility = 1;
     }
 }
