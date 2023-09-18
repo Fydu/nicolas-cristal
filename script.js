affb973b62fa8cde348bf1451d0f792ba203c9ae
Nicolas Cristal <103908372+nicocristal@users.noreply.github.com>
2023-09-18 11:15:51 -0300

    added js for projects

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-var visibility;
+var ListVisibility;
 function showList() {
-    if (visibility == 1) {
+    if (ListVisibility == 1) {
         document.querySelector(".main-list").style.display="none";
         document.getElementById("toggleBtnIcon").classList.add("fa-chevron-down")
         document.getElementById("toggleBtnIcon").classList.remove("fa-chevron-up")
-        return visibility = 0;
+        return ListVisibility = 0;
     }
     else {
         document.querySelector(".main-list").style.display="block";
         document.getElementById("toggleBtnIcon").classList.remove("fa-chevron-down")
         document.getElementById("toggleBtnIcon").classList.add("fa-chevron-up")
-        return visibility = 1;
+        return ListVisibility = 1;
+    }
+}
+
+var ProjectsVisibility;
+function showProjects() {
+    if (ProjectsVisibility == 1) {
+        document.querySelector(".projects-div").style.display="none";
+        return ListVisibility = 0;
+    }
+    else {
+        document.querySelector(".projects-div").style.display="flex";
+        return ListVisibility = 1;
     }
 }
