c646844496e5a5ba3755fc18dcdefe44251a8c44
Nicolas Cristal <103908372+nicocristal@users.noreply.github.com>
2023-09-18 22:21:44 -0300

    added scroll animations

--- script.js
+++
@@ -1,7 +0,0 @@
-function showList() {
-    document.querySelector(".main-list").classList.toggle('active');
-};
-
-function showProjects() {
-    document.querySelector(".projects-div").classList.toggle('active');
-};
