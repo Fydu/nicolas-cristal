b6ce9d51a446df081600cff0f8ee0053f0ab275b
Nicolas Cristal <103908372+nicocristal@users.noreply.github.com>
2023-09-19 12:45:52 -0300

    added to-top button and section gaps

--- scroll.js
+++ scroll.js
@@ -11,3 +11,12 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+});
